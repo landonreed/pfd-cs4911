6127c044650b7f1793c3a73c6f019712a9ec1e12
dmurray912@gmail.com <dmurray912@gmail.com@79b5e1e1-785f-a389-d3a5-1a02245ebd73>
2012-06-04 21:03:58 +0000

    Added all the project files.

---
+++ trunk/ pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/AboutPage.java
@@ -0,0 +1,16 @@
+package com.lifePreserverDiet.PFD.UserInterface;
+
+import com.lifePreserverDiet.PFD.R;
+import com.lifePreserverDiet.PFD.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutPage extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
