f0c81e0b6ab3a3a99bcbdc5e52584084a51042c8
lsissoko91 <lsissoko91@gmail.com>
2012-06-26 00:53:07 +0000

    - Added getters and setters to Day class - Created DayDataSource and MySQLiteHelper classes

---  pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/Day.java
+++  pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/Day.java
@@ -1,15 +1,51 @@
 package com.lifePreserverDiet.PFD;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Day {
-	Date date;
+
+	private long id; // db column id
+	private Date date;
+	private int wholeGrains, dairy, meatBeans, fruit, veggies, extra, exercise;
+
+	@Override
+	public String toString() {
+		return new SimpleDateFormat("EEEE, dd MMMM yyyy, hh:mm:ss.SSS a").format(date);
+	}
 	
-	int wholeGrains;
-	int dairy;
-	int meatBeans;
-	int fruit;
-	int veggies;
-	int extra;
-	int exercise;
+	public Date getDate(){ return date; }
+	public void setDate(String date){
+		try{
+			this.date = new SimpleDateFormat("EEEE, dd MMMM yyyy, hh:mm:ss.SSS a").parse(date);
+		}catch (ParseException e){
+			e.printStackTrace();
+		}
+	}
+
+	public long getId(){ return id; }
+	public void setId(long id){ this.id = id; }
+	
+	public int getWholeGrains(){ return wholeGrains; }
+	public void setWholeGrains(int wg){ wholeGrains = wg; }
+	
+	public int getDairy(){ return dairy; }
+	public void setDairy(int dairy){ this.dairy = dairy; }
+	
+	public int getMeatBeans(){ return meatBeans; }
+	public void setMeatBeans(int meatBeans){ this.meatBeans = meatBeans; }
+	
+	public int getFruit(){ return fruit; }
+	public void setFruit(int fruit){ this.fruit = fruit; }
+	
+	public int getVeggies(){ return veggies; }
+	public void setVeggies(int veggies){ this.veggies = veggies; }
+	
+	public int getExtra(){ return extra; }
+	public void setExtra(int extra){ this.extra = extra; }
+	
+	public int getExercise(){ return exercise; }
+	public void setExercise(int exercise){ this.exercise = exercise; }
 }
---
+++  pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/MySQLiteHelper.java
@@ -0,0 +1,53 @@
+package com.lifePreserverDiet.PFD;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class MySQLiteHelper extends SQLiteOpenHelper{
+	public static final String TABLE_DAYS = "days",
+			COLUMN_ID = "_id",
+			COLUMN_DATE = "date",
+			COLUMN_WHOLEGRAINS = "wholeGrains",
+			COLUMN_DAIRY = "dairy",
+			COLUMN_MEATBEANS = "meatBeans",
+			COLUMN_FRUIT = "fruit",
+			COLUMN_VEGGIES = "veggies",
+			COLUMN_EXTRA = "extra",
+			COLUMN_EXERCISE = "exercise",
+			DATABASE_NAME = "days.db";
+	private static final int DATABASE_VERSION = 1;
+	
+	// Database creation sql statement
+	private static final String DATABASE_CREATE = "create table " + TABLE_DAYS
+			+ "("
+			+ COLUMN_ID + " integer primary key autoincrement, "
+			+ COLUMN_DATE + " text not null, "
+			+ COLUMN_WHOLEGRAINS + " integer, "
+			+ COLUMN_DAIRY + " integer, "
+			+ COLUMN_MEATBEANS + " integer, "
+			+ COLUMN_FRUIT + " integer, "
+			+ COLUMN_VEGGIES + " integer, "
+			+ COLUMN_EXTRA + " integer, "
+			+ COLUMN_EXERCISE + " integer "
+			+ ");";
+
+	public MySQLiteHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(MySQLiteHelper.class.getName(),
+				"Upgrading database from version " + oldVersion + " to "
+						+ newVersion + ", which will destroy all old data");
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_DAYS);
+		onCreate(db);
+	}
+}
