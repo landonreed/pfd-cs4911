c80ca7f5e20963269bca330011e66b8fd5e88266
lsissoko91@gmail.com <lsissoko91@gmail.com@79b5e1e1-785f-a389-d3a5-1a02245ebd73>
2012-06-26 00:53:07 +0000

    - Added getters and setters to Day class - Created DayDataSource and MySQLiteHelper classes

---
+++ trunk/ pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/DayDataSource.java
@@ -0,0 +1,120 @@
+package com.lifePreserverDiet.PFD;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DayDataSource {
+
+	/** Database fields */
+	private SQLiteDatabase database;
+	private MySQLiteHelper dbHelper;
+	private String[] allColumns = { 
+			MySQLiteHelper.COLUMN_ID,
+			MySQLiteHelper.COLUMN_DATE,
+			MySQLiteHelper.COLUMN_WHOLEGRAINS,
+			MySQLiteHelper.COLUMN_DAIRY,
+			MySQLiteHelper.COLUMN_MEATBEANS,
+			MySQLiteHelper.COLUMN_FRUIT,
+			MySQLiteHelper.COLUMN_EXTRA,
+			MySQLiteHelper.COLUMN_EXERCISE
+	};
+
+	public DayDataSource(Context context) {
+		dbHelper = new MySQLiteHelper(context);
+	}
+
+	public void open() throws SQLException {
+		database = dbHelper.getWritableDatabase();
+	}
+
+	public void close() {
+		dbHelper.close();
+	}
+	
+	/**
+	 * Inserts a new Day into the table
+	 */
+	public Day createDay(Date date, int wholeGrains, int dairy, int meatBeans,
+			int fruit, int veggies, int extra, int exercise)
+	{
+		ContentValues values = new ContentValues();
+		
+		values.put(MySQLiteHelper.COLUMN_DATE, 
+				new SimpleDateFormat("EEEE, dd MMMM yyyy, hh:mm:ss.SSS a").format(date));
+		values.put(MySQLiteHelper.COLUMN_WHOLEGRAINS, ((Integer)wholeGrains).toString());
+		values.put(MySQLiteHelper.COLUMN_DAIRY, ((Integer)dairy).toString());
+		values.put(MySQLiteHelper.COLUMN_MEATBEANS, ((Integer)meatBeans).toString());
+		values.put(MySQLiteHelper.COLUMN_FRUIT, ((Integer)fruit).toString());
+		values.put(MySQLiteHelper.COLUMN_VEGGIES, ((Integer)veggies).toString());
+		values.put(MySQLiteHelper.COLUMN_EXTRA, ((Integer)extra).toString());
+		values.put(MySQLiteHelper.COLUMN_EXERCISE, ((Integer)exercise).toString());
+		
+		long insertId = database.insert(MySQLiteHelper.TABLE_DAYS, null,
+				values);
+		
+		Cursor cursor = database.query(MySQLiteHelper.TABLE_DAYS,
+				allColumns, MySQLiteHelper.COLUMN_ID + " = " + insertId, null,
+				null, null, null);
+		
+		cursor.moveToFirst();
+		Day newDay = cursorToDay(cursor);
+		cursor.close();
+		
+		return newDay;
+	}
+
+	/**
+	 * Removes a Day from the table
+	 */
+	public void deleteDay(Day day) {
+		long id = day.getId();		
+		System.out.println("Day deleted with id: " + id);		
+		database.delete(MySQLiteHelper.TABLE_DAYS, MySQLiteHelper.COLUMN_ID
+				+ " = " + id, null);
+	}
+
+	/**
+	 * Returns a list of all Days contained in the table
+	 */
+	public List<Day> getAllDays() {
+		List<Day> days = new ArrayList<Day>();
+
+		Cursor cursor = database.query(MySQLiteHelper.TABLE_DAYS,
+				allColumns, null, null, null, null, null);
+
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()){
+			Day day = cursorToDay(cursor);
+			days.add(day);
+			cursor.moveToNext();
+		}
+		cursor.close();
+		
+		return days;
+	}
+
+	/**
+	 * Returns the table row pointed to by the given cursor as a Day object
+	 */
+	private Day cursorToDay(Cursor cursor) {
+		Day day = new Day();
+		day.setId(cursor.getLong(0));
+		day.setDate(cursor.getString(1));
+		day.setWholeGrains(cursor.getInt(2));
+		day.setDairy(cursor.getInt(3));
+		day.setMeatBeans(cursor.getInt(4));
+		day.setFruit(cursor.getInt(5));
+		day.setVeggies(cursor.getInt(6));
+		day.setExtra(cursor.getInt(7));
+		day.setExercise(cursor.getInt(8));
+		return day;
+	}
+}
