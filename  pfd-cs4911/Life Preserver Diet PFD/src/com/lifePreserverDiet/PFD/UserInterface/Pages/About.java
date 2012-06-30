24e90ac3ebcc0d98db6f6eda4b1b213bf9dd4505
dmurray912 <dmurray912@gmail.com>
2012-06-30 20:11:43 +0000

    Updated the interface to what Marcia wants.

---
+++  pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/About.java
@@ -0,0 +1,14 @@
+package com.lifePreserverDiet.PFD.UserInterface.Pages;
+
+import com.lifePreserverDiet.PFD.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		setContentView(R.layout.about);
+	}
+}
