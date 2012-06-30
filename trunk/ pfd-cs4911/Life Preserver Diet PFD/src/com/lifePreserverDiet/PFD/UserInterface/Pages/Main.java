662ac52c0ec11c2b923aee9a33f17b98d32feb51
dmurray912@gmail.com <dmurray912@gmail.com@79b5e1e1-785f-a389-d3a5-1a02245ebd73>
2012-06-30 20:11:43 +0000

    Updated the interface to what Marcia wants.

---
+++ trunk/ pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/Main.java
@@ -0,0 +1,42 @@
+package com.lifePreserverDiet.PFD.UserInterface.Pages;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.lifePreserverDiet.PFD.R;
+
+public class Main extends Activity {
+
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		setContentView(R.layout.main);
+	}
+	
+	public void personalFlotationDevice(View v) {
+		Intent intent = new Intent(this, PersonalFlotationDevice.class);
+		startActivity(intent);
+	}
+	
+	public void captainsLog(View v) {
+		Intent intent = new Intent(this, CaptainsLog.class);
+		startActivity(intent);
+	}
+	
+	public void book(View v) {
+		Intent intent = new Intent(this, Book.class);
+		startActivity(intent);
+	}
+	
+	public void about(View v) {
+		Intent intent = new Intent(this, About.class);
+		startActivity(intent);
+	}
+	
+	public void contact(View v) {
+		Intent intent = new Intent(this, Contact.class);
+		startActivity(intent);
+	}
+	
+}
