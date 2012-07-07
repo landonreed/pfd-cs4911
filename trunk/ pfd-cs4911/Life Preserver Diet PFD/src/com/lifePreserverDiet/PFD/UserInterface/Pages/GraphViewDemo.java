dab5dd51ab212d33b6d493ca7a64063c55f111c5
lsissoko91@gmail.com <lsissoko91@gmail.com@79b5e1e1-785f-a389-d3a5-1a02245ebd73>
2012-07-07 23:57:12 +0000

    Added basic graph. Here's the jar to import to get access to the GraphView classes: https://github.com/downloads/jjoe64/GraphView/GraphView-2.5.jar

---
+++ trunk/ pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/GraphViewDemo.java
@@ -0,0 +1,108 @@
+package com.lifePreserverDiet.PFD.UserInterface.Pages;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphView.GraphViewSeries;
+import com.jjoe64.graphview.BarGraphView;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphView.LegendAlign;
+import com.jjoe64.graphview.LineGraphView;
+
+import com.lifePreserverDiet.PFD.R;
+
+/**
+ * GraphViewDemo creates some dummy data to demonstrate the GraphView component.
+ *
+ * IMPORTANT: For examples take a look at GraphView-Demos (https://github.com/jjoe64/GraphView-Demos)
+ *
+ * Copyright (C) 2011 Jonas Gehring
+ * Licensed under the GNU Lesser General Public License (LGPL)
+ * http://www.gnu.org/licenses/lgpl.html
+ */
+public class GraphViewDemo extends Activity {
+	/**
+	 * @param savedInstanceState
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.graphs);
+
+		// init example series data
+		GraphViewSeries exampleSeries = new GraphViewSeries(
+			"Series 1",
+			null,
+			new GraphViewData[] {
+				new GraphViewData(0, 0.0d) // need to create a point for the origin
+				, new GraphViewData(1, 2.0d)
+				, new GraphViewData(2, 1.5d)
+				, new GraphViewData(2.5, 3.0d) // another frequency
+				, new GraphViewData(3, 2.5d)
+				, new GraphViewData(4, 6.0d)
+				, new GraphViewData(5, 3.0d)
+				, new GraphViewData(12, 2.3d) // point beyond viewpoint (to test scrolling)
+		});
+
+		// graph with dynamically generated horizontal and vertical labels
+		GraphView graphView;
+		if (getIntent().getStringExtra("type").equals("bar"))
+			graphView = new BarGraphView(this, "Graph Title");
+		else
+			graphView = new LineGraphView(this, "Graph Title");
+		
+		/*
+		GraphView graphView = new LineGraphView(this, "example") {
+			@Override
+			protected String formatLabel(double value, boolean isValueX) {
+				// convert unix time to human time
+				if (isValueX)
+			        return dateTimeFormatter.format(new Date((long) value*1000));
+				// let the y-value be normal-formatted
+				else
+			    	return super.formatLabel(value, isValueX); 
+			}
+		};*/
+		
+		// add the data
+		graphView.addSeries(exampleSeries);
+		
+		// view bounds
+		graphView.setViewPort(0, 4);
+		graphView.setManualYAxisBounds(10, 0);
+		
+		// zoom and scale
+		graphView.setScrollable(true);
+		//graphView.setScalable(true);
+		
+		// legend
+		graphView.setShowLegend(true);
+		graphView.setLegendAlign(LegendAlign.BOTTOM);  
+		graphView.setLegendWidth(100);
+
+		LinearLayout layout = (LinearLayout) findViewById(R.id.graph1);
+		//layout.removeAllViews(); // replace old graph with new one
+		layout.addView(graphView);
+
+		//
+		// graph with custom labels and drawBackground
+		//
+		if (getIntent().getStringExtra("type").equals("bar"))
+			graphView = new BarGraphView(this, "Graph Title");
+		else{
+			graphView = new LineGraphView(this, "Graph Title");
+			((LineGraphView) graphView).setDrawBackground(true);
+		}
+		
+		// custom static labels
+		graphView.setHorizontalLabels(
+				new String[] {"2 days ago", "yesterday", "today", "tomorrow"});
+		graphView.setVerticalLabels(new String[] {"high", "middle", "low"});
+		graphView.addSeries(exampleSeries); // data
+
+		layout = (LinearLayout) findViewById(R.id.graph2);
+		layout.addView(graphView);
+	}
+}
--- trunk/ pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/TestDatabaseActivity.java
+++ trunk/ pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/TestDatabaseActivity.java
@@ -1,18 +1,33 @@
 package com.lifePreserverDiet.PFD.UserInterface.Pages;
 
-import java.util.List;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.*;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.androidplot.Plot;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.series.XYSeries;
+import com.androidplot.xy.*;
 
 import com.lifePreserverDiet.PFD.Day;
 import com.lifePreserverDiet.PFD.DayDataSource;
 import com.lifePreserverDiet.PFD.R;
-
-import android.app.ListActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
+ 
+import java.text.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class TestDatabaseActivity extends ListActivity {
 	private DayDataSource datasource;
+	
+	private XYPlot mySimpleXYPlot;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,8 +44,95 @@ public class TestDatabaseActivity extends ListActivity {
 				new ArrayAdapter<Day>(this, android.R.layout.simple_list_item_1, values);
 		
 		setListAdapter(adapter);
+		
+		
+		///////////////////////////////////////////////////////////////////////////////////
+
+		/*
+		
+        // initialize our XYPlot reference:
+		mySimpleXYPlot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+		
+        Number[] numSightings = {5, 8, 9, 2, 5};
+        Number[] years = {
+                978307200,  // 2001
+                1009843200, // 2002
+                1041379200, // 2003
+                1072915200, // 2004
+                1104537600  // 2005
+        };
+        // create our series from our array of nums:
+        XYSeries series2 = new SimpleXYSeries(
+                Arrays.asList(years),
+                Arrays.asList(numSightings),
+                "Sightings in USA");
+ 
+        mySimpleXYPlot.getGraphWidget().getGridBackgroundPaint().setColor(Color.WHITE);
+        mySimpleXYPlot.getGraphWidget().getGridLinePaint().setColor(Color.BLACK);
+        mySimpleXYPlot.getGraphWidget().getGridLinePaint().setPathEffect(new DashPathEffect(new float[]{1,1}, 1));
+        mySimpleXYPlot.getGraphWidget().getDomainOriginLinePaint().setColor(Color.BLACK);
+        mySimpleXYPlot.getGraphWidget().getRangeOriginLinePaint().setColor(Color.BLACK);
+ 
+        mySimpleXYPlot.setBorderStyle(Plot.BorderStyle.SQUARE, null, null);
+        mySimpleXYPlot.getBorderPaint().setStrokeWidth(1);
+        mySimpleXYPlot.getBorderPaint().setAntiAlias(false);
+        mySimpleXYPlot.getBorderPaint().setColor(Color.WHITE);
+ 
+        // Create a formatter to use for drawing a series using LineAndPointRenderer:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(
+                Color.rgb(0, 100, 0),                   // line color
+                Color.rgb(0, 100, 0),                   // point color
+                Color.rgb(100, 200, 0));                // fill color
+ 
+ 
+        // setup our line fill paint to be a slightly transparent gradient:
+        Paint lineFill = new Paint();
+        lineFill.setAlpha(200);
+        lineFill.setShader(new LinearGradient(0, 0, 0, 250, Color.WHITE, Color.GREEN, Shader.TileMode.MIRROR));
+ 
+        		// LineAndPointFormatter(line color, point color, fill color)
+        LineAndPointFormatter formatter  = 
+        		new LineAndPointFormatter(Color.rgb(0, 0,0), Color.BLUE, Color.RED);
+        formatter.setFillPaint(lineFill);
+        mySimpleXYPlot.getGraphWidget().setPaddingRight(2);
+        mySimpleXYPlot.addSeries(series2, formatter);
+ 
+        // draw a domain tick for each year:
+        mySimpleXYPlot.setDomainStep(XYStepMode.SUBDIVIDE, years.length);
+ 
+        // customize our domain/range labels
+        mySimpleXYPlot.setDomainLabel("Year");
+        mySimpleXYPlot.setRangeLabel("# of Sightings");
+ 
+        // get rid of decimal points in our range labels:
+        mySimpleXYPlot.setRangeValueFormat(new DecimalFormat("0"));
+ 
+        mySimpleXYPlot.setDomainValueFormat(new Format() { 
+            private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy");
+ 
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos){
+                // our timestamps are in seconds and SimpleDateFormat expects milliseconds
+                long timestamp = ((Number) obj).longValue() * 1000;
+                Date date = new Date(timestamp);
+                return dateFormat.format(date, toAppendTo, pos);
+            }
+ 
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null; 
+            }
+        });
+ 
+        // by default, AndroidPlot displays developer guides to aid in laying out your plot.
+        // To get rid of them call disableAllMarkup():
+        mySimpleXYPlot.disableAllMarkup();
+        */
+		
+
+		//startGraphActivity(GraphViewDemo.class);
 	}
-	
+		
 	public void onClick(View view) {
 		@SuppressWarnings("unchecked")
 		ArrayAdapter<Day> adapter = (ArrayAdapter<Day>) getListAdapter();
@@ -79,5 +181,10 @@ public class TestDatabaseActivity extends ListActivity {
 	protected void onPause() {
 		datasource.close();
 		super.onPause();
+	}
+	
+	private void startGraphActivity(Class<? extends Activity> activity) {
+		Intent intent = new Intent(TestDatabaseActivity.this, activity);
+		startActivity(intent);
 	}
 }
