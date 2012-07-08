fdd66220b4a97e5f7ea81528809e7380453be33a
lsissoko91@gmail.com <lsissoko91@gmail.com@79b5e1e1-785f-a389-d3a5-1a02245ebd73>
2012-07-08 01:03:01 +0000

    Example of how we could show a target goal (total shares for a given day) in RED and the actual achievement (actual shares for a given day) in BLUE.

--- trunk/ pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/GraphViewDemo.java
+++ trunk/ pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/GraphViewDemo.java
@@ -1,6 +1,7 @@
 package com.lifePreserverDiet.PFD.UserInterface.Pages;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
@@ -8,6 +9,7 @@ import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphView.GraphViewSeries;
 import com.jjoe64.graphview.BarGraphView;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphView.GraphViewStyle;
 import com.jjoe64.graphview.GraphView.LegendAlign;
 import com.jjoe64.graphview.LineGraphView;
 
@@ -31,11 +33,7 @@ public class GraphViewDemo extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.graphs);
 
-		// init example series data
-		GraphViewSeries exampleSeries = new GraphViewSeries(
-			"Series 1",
-			null,
-			new GraphViewData[] {
+		GraphViewData[] actualData = new GraphViewData[] {
 				new GraphViewData(0, 0.0d) // need to create a point for the origin
 				, new GraphViewData(1, 2.0d)
 				, new GraphViewData(2, 1.5d)
@@ -44,8 +42,19 @@ public class GraphViewDemo extends Activity {
 				, new GraphViewData(4, 6.0d)
 				, new GraphViewData(5, 3.0d)
 				, new GraphViewData(12, 2.3d) // point beyond viewpoint (to test scrolling)
-		});
+		};
 
+		GraphViewData[] idealData = new GraphViewData[] {
+				new GraphViewData(0, 0.0d) // need to create a point for the origin
+				, new GraphViewData(1, 3.0d)
+				, new GraphViewData(2, 2.5d)
+				, new GraphViewData(2.5, 3.0d) // another frequency
+				, new GraphViewData(3, 3.5d)
+				, new GraphViewData(4, 7.0d)
+				, new GraphViewData(5, 5.0d)
+				, new GraphViewData(12, 2.6d) // point beyond viewpoint (to test scrolling)
+		};
+		
 		// graph with dynamically generated horizontal and vertical labels
 		GraphView graphView;
 		if (getIntent().getStringExtra("type").equals("bar"))
@@ -67,7 +76,9 @@ public class GraphViewDemo extends Activity {
 		};*/
 		
 		// add the data
-		graphView.addSeries(exampleSeries);
+		graphView.addSeries(new GraphViewSeries("Target",
+				new GraphViewStyle(Color.rgb(150, 62, 35), 3), idealData));
+		graphView.addSeries(new GraphViewSeries("Actual", null, actualData));
 		
 		// view bounds
 		graphView.setViewPort(0, 4);
@@ -95,6 +106,9 @@ public class GraphViewDemo extends Activity {
 			graphView = new LineGraphView(this, "Graph Title");
 			((LineGraphView) graphView).setDrawBackground(true);
 		}
+		
+		// init example series data
+		GraphViewSeries exampleSeries = new GraphViewSeries("Series 1", null, actualData);
 		
 		// custom static labels
 		graphView.setHorizontalLabels(
