e03e20f0fd21dd04f4a5229cb8945551d4116408
dmurray912 <dmurray912@gmail.com>
2012-07-17 01:41:41 +0000

    Various changes made today before meeting Marcia.

---  pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/About.java
+++  pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/About.java
@@ -1,14 +1,21 @@
 package com.lifePreserverDiet.PFD.UserInterface.Pages;
 
-import com.lifePreserverDiet.PFD.R;
-
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import com.lifePreserverDiet.PFD.R;
 
 public class About extends Activity {
 
 	public void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		setContentView(R.layout.page_about_diet);
+	}
+	
+	public void youtube(View v) {
+		startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=TIZeFqYaEE0")));
 	}
 }
