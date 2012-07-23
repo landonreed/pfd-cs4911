ef5826023cc32736266abb01c948e5e366cadfee
dmurray912 <dmurray912@gmail.com>
2012-07-23 04:19:12 +0000

    Added color to the data table on the PFD.

---  pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/PersonalFlotationDevice.java
+++  pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/PersonalFlotationDevice.java
@@ -2,6 +2,7 @@ package com.lifePreserverDiet.PFD.UserInterface.Pages;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -57,45 +58,86 @@ public class PersonalFlotationDevice extends Activity {
 		
 		if(day.getExercise()) {
 			exerciseShares.setText("Y");
+			exerciseShares.setTextColor(Color.GREEN);
+			exerciseTimeShares.setTextColor(Color.GREEN);
 			exerciseTimeShares.setText(day.getExerciseMinutes() + " min");
 		} else {
+			day.setExerciseMinutes(0);
 			exerciseShares.setText("N");
+			exerciseShares.setTextColor(Color.BLACK);
+			exerciseTimeShares.setTextColor(Color.BLACK);
 			exerciseTimeShares.setText("0 min");
 		}
 		
 		if(day.getMeatBeans() >= 3) {
+			if(day.getMeatBeans() > 3) {
+				meatBeansShares.setTextColor(Color.RED);
+			} else {
+				meatBeansShares.setTextColor(Color.GREEN);
+			}
 			meatBeansShares.setText("0");
 		} else {
+			meatBeansShares.setTextColor(Color.BLACK);
 			meatBeansShares.setText((3-day.getMeatBeans())+"");
 		}
 		
 		if(day.getDairy() >= 3) {
+			if(day.getDairy() > 3) {
+				dairyShares.setTextColor(Color.RED);
+			} else {
+				dairyShares.setTextColor(Color.GREEN);
+			}
 			dairyShares.setText("0");
 		} else {
+			dairyShares.setTextColor(Color.BLACK);
 			dairyShares.setText((3-day.getDairy())+"");
 		}
 		
 		if(day.getVeggies() >= 4) {
+			if(day.getVeggies() > 4) {
+				veggiesShares.setTextColor(Color.RED);
+			} else {
+				veggiesShares.setTextColor(Color.GREEN);
+			}
 			veggiesShares.setText("0");
 		} else {
+			veggiesShares.setTextColor(Color.BLACK);
 			veggiesShares.setText((4-day.getVeggies())+"");
 		}
 		
 		if(day.getExtra() >= 3) {
+			if(day.getExtra() > 3) {
+				extraShares.setTextColor(Color.RED);
+			} else {
+				extraShares.setTextColor(Color.GREEN);
+			}
 			extraShares.setText("0");
 		} else {
+			extraShares.setTextColor(Color.BLACK);
 			extraShares.setText((3-day.getExtra())+"");
 		}
 		
 		if(day.getFruit() >= 3) {
+			if(day.getFruit() > 3) {
+				fruitsShares.setTextColor(Color.RED);
+			} else {
+				fruitsShares.setTextColor(Color.GREEN);
+			}
 			fruitsShares.setText("0");
 		} else {
+			fruitsShares.setTextColor(Color.BLACK);
 			fruitsShares.setText((3-day.getFruit())+"");
 		}
 		
 		if(day.getWholeGrains() >= 3) {
+			if(day.getWholeGrains() > 3) {
+				wholeGrainsShares.setTextColor(Color.RED);
+			} else {
+				wholeGrainsShares.setTextColor(Color.GREEN);
+			}
 			wholeGrainsShares.setText("0");
 		} else {
+			wholeGrainsShares.setTextColor(Color.BLACK);
 			wholeGrainsShares.setText((3-day.getWholeGrains())+"");
 		}
 		
