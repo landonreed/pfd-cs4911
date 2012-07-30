1cb7650232fb32d156dfa82fe65b5408afebdb2a
devinwang91 <devinwang91@gmail.com>
2012-07-30 05:59:34 +0000

    Implemented the animated splash screen for the day after a perfect score (need to change it so that it only does it on the first time the user visits the app that day). I set CelebrationSplash as the Launcher activity, but it only plays the animation if the user had a score of at least 20 the day before. Otherwise, it just goes straight to Splash (the normal splash screen). Also, I'm aware the new Veggies dialog runs over, I'll shorten it tomorrow.

---
+++  pfd-cs4911/Life Preserver Diet PFD/src/com/lifePreserverDiet/PFD/UserInterface/Pages/CelebrationSplash.java
@@ -0,0 +1,90 @@
+package com.lifePreserverDiet.PFD.UserInterface.Pages;
+
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.lifePreserverDiet.PFD.Day;
+import com.lifePreserverDiet.PFD.R;
+import com.lifePreserverDiet.PFD.UserInterface.LifePreserverDiet;
+import com.lifePreserverDiet.PFD.Utilities.DayDataSource;
+
+public class CelebrationSplash extends Activity {
+	
+	private Thread mSplashThread;
+	private Date mydate;
+	private DayDataSource datasource;
+	private Day d;
+
+	public void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+		
+		datasource = new DayDataSource(this);
+		datasource.open();
+		
+		mydate = new Date();
+		mydate.setTime(mydate.getTime() - (24*60*60*1000));
+		
+		d = datasource.getDay(mydate);
+		double wholeGrainsTotal, dairyTotal, meatBeansTotal, fruitTotal, extraTotal;
+		wholeGrainsTotal = dairyTotal = meatBeansTotal = fruitTotal = extraTotal = 3.0;
+		if(d != null){
+			double exerciseShare = (d.getExerciseMinutes() > 0) ? 1.0 : 0.0;
+			double total =
+					wholeGrainsTotal - Math.abs(wholeGrainsTotal - d.getWholeGrains()) +
+					dairyTotal - Math.abs(dairyTotal - d.getDairy()) + 
+					meatBeansTotal - Math.abs(meatBeansTotal - d.getMeatBeans()) +
+					fruitTotal - Math.abs(fruitTotal - d.getFruit()) + 
+					d.getVeggies() + 
+					extraTotal - Math.abs(extraTotal - d.getExtra()) +
+					exerciseShare;
+			if(total >= 20.0){
+				setContentView(R.layout.page_splash_congrats);
+				final CelebrationSplash splashScreen = this;
+				mSplashThread = new Thread(){
+					public void run(){
+						try{
+							synchronized(this){
+								wait(4000);
+							}
+						}
+						catch(InterruptedException ex){
+						}
+						finish();
+						Intent intent = new Intent();
+						intent.setClass(splashScreen,  Splash.class);
+						startActivity(intent);
+						stop();
+					}
+				};
+				mSplashThread.start();
+				
+				final ImageView img = (ImageView)findViewById(R.id.congrats_splash);
+				img.setBackgroundResource(R.drawable.celebration_splash);
+				// Get the background, which has been compiled to an AnimationDrawable object.
+				final AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+				if(true){
+					img.post(new Runnable(){
+						public void run(){
+							frameAnimation.start();
+						}
+					});
+				}
+			}
+			else{
+				Intent intent = new Intent(CelebrationSplash.this, Splash.class);
+				CelebrationSplash.this.startActivity(intent);
+				CelebrationSplash.this.finish();
+			}
+		}
+		else{
+			Intent intent = new Intent(CelebrationSplash.this, Splash.class);
+			CelebrationSplash.this.startActivity(intent);
+			CelebrationSplash.this.finish();
+		}
+	}
+}
